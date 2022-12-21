b893254bc5f97fa34c0311f680e6dffdb46c59f4
Alba Aranda <albaarandagomez@gmail.com>
2022-12-21 18:20:05 +0000

    modificacion laravel

--- app/Http/Controllers/AsignaturaController.php
+++ app/Http/Controllers/AsignaturaController.php
@@ -37,9 +37,65 @@ class AsignaturaController extends Controller
     public function store(Request $request)
     {
         //recoge los datos del formulario de alta
+        //aqui iria la lógica de insertar en la bbdd. save (en el modelo)
+        //dd("HE LLEGADO AQUÍ");  //dd es como un echo
 
-        dd("HE LLEGADO AQUÍ");  //dd es como un echo
+        $datos = $request->validate([
+            'nombre' => 'required|max:7',
+            'curso' => 'required|integer|size:1|regex:/[1,2]/',
+            'ciclo' => 'required|size:3|regex:/DA[M,W]/'
+        ],[
+            'nombre.required'=> 'Debes rellenar el nombre',
+            'nombre.max'=> 'El nombre solo puede tener un máximo de 7 caracteres',
+            'curso.required'=> 'Debes rellenar el curso',
+            'curso.integer'=> 'El curso debe ser un número entero',
+            'curso.regex'=> 'El curso debe estar comprendido entre 1 y 2',
+            'curso.size' => 'El curso debe tener solo un número entero',
+            'ciclo.required'=> 'Debes rellenar el ciclo',
+            'ciclo.regex' => 'El ciclo debe ser DAM O DAW',
+            'ciclo.size' => 'El ciclo solo pueden contener 3 letras'
+        ]);
+
+        /*
+        $nombre = $request->input('nombre'); //se recomienda esta porque permite utilizar array  Hay otras opciones
+        $curso = $request->input('curso');
+        $ciclo = $request->input('ciclo');
+        $comentario = $request->input('comentario');
+        
+        dd($nombre, $curso, $ciclo, $comentario); // para mostrar estas variables
+
+        */
+
+
+        /*
+        $datos = $request->all();
+        dd($datos);
+        */
+
+        /*
+        $datos = $request->only('nombre','ciclo');
+        dd($datos);
+        */
+
+        /*
+        $datos = $request->except('nombre');
+        dd($datos);
+        */
+
+        //verificar que existe un campo en el formulario
+
+        /*
+        $nuevocampo = "";
+        if($request->has('nuevocampo')){
+            dd($nuevocampo);
+        }
+        else{
+            dd("El campo no existe");
+        }
+        */
     }
+
+
 
     /**
      * Display the specified resource.
--- resources/views/asignaturas/create.blade.php
+++ resources/views/asignaturas/create.blade.php
@@ -6,18 +6,30 @@
     Alta de asignaturas
 @stop
 
+<!--Para recoger los errores -->
 @section('cuerpo')
     @parent
+    @if($errors->any())
+    <div class="alert alert-danger">
+        <h6>Por favor corrige los siguientes errores</h6>
+        <ul>
+        @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+        </ul>
+    </div>
+    @endif
     <br>Completa el siguiente formulario:
     <form action="{{route('asignaturas.store')}}" method="post">
+    @csrf <!--Esto se pone por seguridad. lo malicioiso, se puede hacer un ataque con -> cross-site request forgery. Hay que estar logueado para poder hacer esto. Con este arroba se añade un token, un valor en el input-->
     <label for="nombre">Nombre</label><br>
-    <input type="text" name="nombre" id="nombre">
+    <input type="text" name="nombre" id="nombre" value="{{old('nombre') }}"> <!--Con el contenido en value se queda lo escrito en el input -->
     <br><br>
     <label for="curso">Curso</label><br>
-    <input type="text" name="curso" id="curso">
+    <input type="text" name="curso" id="curso" value="{{old('curso') }}">
     <br><br>
     <label for="ciclo">Ciclo</label><br>
-    <input type="text" name="ciclo" id="ciclo">
+    <input type="text" name="ciclo" id="ciclo" value="{{old('ciclo') }}">
     <br><br>
     <label for="comentario">Comentario</label><br>
     <textarea name="comentario" id="comentarios" cols="30" rows="10" placeholder="Escribe aquí"></textarea>
@@ -32,7 +44,7 @@
     @stop
 
     @section('accionformulario')
-        Enviar
+    Enviar
     @stop
     </form>
 @stop
